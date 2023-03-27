b2c01fcabbf9b87d9fb7c3b42e0f1ed637f3df38
Soroush Habibi <soroush.habibi.1382@gmail.com>
2023-03-27 14:10:46 +0330

    make early structure for backend

--- backend/build/index.js
+++ backend/build/index.js
@@ -1,13 +1,9 @@
 import express from 'express';
-
 import getRouter from './routes/getRouter.js';
-
 const app = express();
-
 //------------------middlewares------------------
 app.use(express.json());
 app.use(getRouter);
-
 app.listen(process.env.BACKEND_PORT || 3001, () => {
     console.log(`Backend is running on port ${process.env.BACKEND_PORT || 3001}`);
 });
